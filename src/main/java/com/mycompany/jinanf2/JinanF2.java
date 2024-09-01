16d739f2d8096191a86220b328811862ff09bc44
Jinan Fahad <jbalmidfa@stu.kau.edu.sa>
2024-09-01 21:17:30 +0300

    Test Commit

---
+++ src/main/java/com/mycompany/jinanf2/JinanF2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jinanf2;
+
+/**
+ *
+ * @author jinanf.
+ */
+public class JinanF2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Software Engineering !");
+        System.out.println("I'm Jinan !");
+
+    }
+}
